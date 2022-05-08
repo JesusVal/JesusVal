b263047e9218eb1d00d8f8e1b86c59e069f618db
Jesus Valadez <jesus.valadez.f.97@gmail.com>
2022-05-08 18:39:52 -0500

    Experience component created

--- src/components/resume/Resume.jsx
+++ src/components/resume/Resume.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import About from "./resources/about/About";
+import Experience from "./resources/experience/Experience";
 import Header from "./resources/header/Header";
 import Navbar from "./resources/navbar/Navbar";
 
@@ -9,6 +10,7 @@ const Resume = () => {
       <Header />
       <Navbar />
       <About />
+      <Experience />
     </>
   );
 };
